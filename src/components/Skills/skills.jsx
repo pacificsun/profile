c266090e09cca220338fb549b8e7abc82f772dac
Suraj Thapa <stsurajst@gmail.com>
2023-10-16 22:33:04 -0500

    skills page complete

---
+++ src/components/Skills/skills.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import './skills.css'
+
+import UIDesign from '../../assets/ui-design.png';
+import WebDesign from '../../assets/website-design.png';
+import AppDesign from '../../assets/app-design.png';
+
+const Skills = () =>{
+
+    return(
+        <section id="skills">
+            <span className="skillTitle">What I do</span>
+            <span className="skillDesc">I am a skilled and passonate software enginer with experienc in web based application.</span>
+            <div className="skillBars">
+                <div className="skillBar">
+                    <img src={UIDesign} alt="UIDesign" className="skillBarImg"/>
+                    <div className="skillBarText">
+                        <h2>UI/UX Design</h2>
+                        <p>This is a demo text, you can write your own content</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={WebDesign} alt="WebDesign" className="skillBarImg"/>
+                    <div className="skillBarText">
+                        <h2>Website Design</h2>
+                        <p>This is a demo text, you can write your own content</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={AppDesign} alt="AppDesign" className="skillBarImg"/>
+                    <div className="skillBarText">
+                        <h2>App Design</h2>
+                        <p>This is a demo text, you can write your own content</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+
+}
+
+export default Skills
