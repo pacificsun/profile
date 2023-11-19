29eefc656250c13657a2bf6494cdcef2c7c2a582
Suraj Thapa <stsurajst@gmail.com>
2023-11-19 11:16:12 -0600

    contact page

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import './contact.css'
+
+import wallmart from '../../assets/walmart.png'
+import mircorsoft from '../../assets/microsoft.png'
+import facebook from '../../assets/facebook.png'
+import adobe from '../../assets/adobe.png'
 const Contact =()=>{
     return(
         <section className="contactPage">
             <div id="clients">
                 Client
-            </div>
+                <h1 className="contactPageTitle">My Clients</h1>
+                <p className="cllientDesc">
+                    I have had the oppurtunity to work with a diverse group of companies.
+
+                </p>
+                <div className="clientImgs">
+                    <img src={wallmart} alt="client" className="clientImg"/>
+                    <img src={mircorsoft} alt="client" className="clientImg"/>
+                    <img src={facebook} alt="client" className="clientImg"/>
+                    <img src={adobe} alt="client" className="clientImg"/>
+                </div>
+            </div> 
             <div id="contact">Contact</div>
         </section>
     )
