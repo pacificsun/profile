f06d7940e7423995cba4e8d7e6e1cf52ee37ab09
Suraj Thapa <stsurajst@gmail.com>
2023-11-20 23:40:12 -0600

    contact form

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -5,11 +5,16 @@ import wallmart from '../../assets/walmart.png'
 import mircorsoft from '../../assets/microsoft.png'
 import facebook from '../../assets/facebook.png'
 import adobe from '../../assets/adobe.png'
+
+import fbIcon from '../../assets/facebook-icon.png'
+import TwitterIcon from '../../assets/twitter.png'
+import YoutubeIcon from '../../assets/youtube.png'
+import InstagramIcon from '../../assets/instagram.png'
+
 const Contact =()=>{
     return(
         <section className="contactPage">
             <div id="clients">
-                Client
                 <h1 className="contactPageTitle">My Clients</h1>
                 <p className="cllientDesc">
                     I have had the oppurtunity to work with a diverse group of companies.
@@ -22,7 +27,23 @@ const Contact =()=>{
                     <img src={adobe} alt="client" className="clientImg"/>
                 </div>
             </div> 
-            <div id="contact">Contact</div>
+            <div id="contact">
+                <h1 className="contactPageTitle">Contact Me</h1> 
+                <span className="contactDesc">Please fill the form below to discuss any work oppurtunities.</span>
+                <form className="contactForm">
+                    <input type="text" className="name" placeholder="Your Name"/>
+                    <input type="email" className = "email" placeholder="Your Email"/>
+                    <textarea name="message" rows="5" placeholder="Your Message" className="msg"></textarea>
+                    <button type="submit" className="submitBtn" value='send'>Submit</button>
+                    <div className="links">
+                        <img src={fbIcon} alt="link" className="link"/>
+                        <img src={TwitterIcon} alt="link" className="link"/>
+                        <img src={YoutubeIcon} alt="link" className="link"/>
+                        <img src={InstagramIcon} alt="link" className="link"/>
+                    </div>
+
+                </form>
+            </div>
         </section>
     )
 }
